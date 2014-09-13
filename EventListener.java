ab6edf0106a852ae83397f03f1ba0edc372bc7c7
CustomCraftDev <undeaD_D@custom-craft.de>
2014-09-13 16:00:11 +0200

    Create EventListener.java

---
+++ EventListener.java
@@ -0,0 +1,26 @@
+package perplayercommand;
+
+import org.bukkit.event.Listener;
+
+public class EventListener implements Listener{
+
+	perplayercommand plugin;
+	
+	/*
+	 *  constructor EventListener
+	 *  @params perplayercommand plugin
+	 */
+	public EventListener(perplayercommand plugin) {
+		this.plugin = plugin;
+	}
+			
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
